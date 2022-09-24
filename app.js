25a4d3fb85daa2ec4f090ae8fc8dbcf2d7987030
faraja17 <farajakthompson@gmail.com>
2022-09-24 13:53:53 -0400

    add ejs files

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
  
 const app = express();
+
+app.set('view engine', 'ejs');
  
 app.get("/", function(req, res) {
     const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let today = new Date();
     let currentDay = weekday[today.getDay()];
         if (currentDay === "Sunday" || currentDay === "Saturday") {
+            res.render("list1", {currentDay: currentDay});
             res.write(`<h1>Yay, it's ${currentDay}! It's the weekend!</h1>`);
         }else{
-            res.sendFile(__dirname + "/index.html");
+            res.render("list", {currentDay: currentDay});
         };
 });
  
