0c1332c18cae720d75542d4320f0172b1841f26b
faraja17 <farajakthompson@gmail.com>
2022-09-24 20:20:14 -0400

    create an array variaiable to store tasks & use for loop + scriptlet tags to add line items

--- app.js
+++ app.js
@@ -4,21 +4,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
  
 const app = express();
+let tasks = ["Study.", "Walk dog.", "Study."];
 
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
  
 app.get("/", function(req, res) {
     let today = new Date();
 
     let options = {
         weekday: "long",
-        // day: "numeric",
-        // month: "long"
+        day: "numeric",
+        month: "long"
     };
 
     let day = today.toLocaleDateString("en-US", options); 
+    res.render("list", {currentDay: day, tasks: tasks});
 
-    res.render("list", {currentDay: day});
+app.post("/", function(req, res) {
+    let task = req.body.task;
+    tasks.push(task);
+    res.redirect("/");
+
+});
+
 });
  
 app.listen(3000, function() {
